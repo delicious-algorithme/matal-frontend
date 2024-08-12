3cd18c1ace7b67e159898f6f65ebfaf773dcbd76
hae_oo <songhj10207@naver.com>
2024-08-12 22:27:30 +0900

    Fix: Map currentLocation 추가해서 디폴트 값 설정

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -2,18 +2,24 @@ import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 const MyMap = () => {
+    const currentLocation = {
+        lat: 37.3595704,
+        lng: 127.105399,
+    };
     const { naver } = window;
     useEffect(() => {
-        const map = new naver.maps.Map('map', {
-            center: new naver.maps.LatLng(37.3595704, 127.105399),
-            zoomControl: true,
-            zoomControlOptions: {
-                style: naver.maps.ZoomControlStyle.SMALL,
-                position: naver.maps.Position.TOP_RIGHT,
-            },
-            zoom: 10,
-        });
-    }, []);
+        if (currentLocation.lat !== 0 && currentLocation.lng !== 0) {
+            const map = new naver.maps.Map('map', {
+                center: new naver.maps.LatLng(37.3595704, 127.105399),
+                zoomControl: true,
+                zoomControlOptions: {
+                    style: naver.maps.ZoomControlStyle.SMALL,
+                    position: naver.maps.Position.TOP_RIGHT,
+                },
+                zoom: 10,
+            });
+        }
+    }, [currentLocation]);
     return <MapContatiner id="map" style={{ width: '100%', height: '100%' }} />;
 };
 export default MyMap;
