f4627c1825e59c01c1b0b1a8e539489cc3582f37
hae_oo <songhj10207@naver.com>
2024-08-26 02:49:41 +0900

    fix : 카테고리 수정하고 idx-> item으로 수정 #23
     카테고리 수정하고 idx-> item으로 수정

--- src/components/common/Category.jsx
+++ src/components/common/Category.jsx
@@ -3,26 +3,54 @@ import { Orange } from '../../color';
 import { White } from '../../color';
 import { useNavigate } from 'react-router-dom';
 const items = [
+    '한정식',
+    '일식당',
+    '양식',
+    '중식당',
+    '카페,디저트',
+    '베이커리',
+    '칼국수,만두',
     '냉면',
-    '파스타',
-    '삼겹살',
-    '스테이크',
-    '초밥',
+    '기사식당',
+    '한식',
+    '백반,가정식',
+    '생선구이',
+    '육류,고기요리',
+    '두부요리',
+    '국밥',
+    '주꾸미요리',
+    '정육식당',
+    '보리밥',
+    '요리주점',
+    '찌개,전골',
+    '닭갈비',
+    '맥주,호프',
+    '인도음식',
+    '카레',
+    '초밥,롤',
+    '돈가스',
     '떡볶이',
-    '빵',
-    '부대찌개',
-    '돈까스',
-    '수제햄버거',
-    '짜장면',
-    '마라탕',
-    '김치찌개',
+    '종합분식',
+    '조개요리',
+    '일본식라면',
     '덮밥',
+    '베트남음식',
+    '양꼬치',
+    '생선회',
+    '순대,순댓국',
+    '샤브샤브',
+    '이탈리아음식',
+    '스파게티,파스타전문',
+    '이자카야',
+    '돼지고기구이',
+    '태국음식',
+    '아시아음식',
 ];
 
 const Category = (position) => {
     const navigate = useNavigate();
-    const onClickHandler = (idx) => {
-        navigate(`/webmap/storeList/${idx}`, { state: { listVisible: true } }); //임시 경로
+    const onClickHandler = (item) => {
+        navigate(`/webmap/storeList/${item}`, { state: { listVisible: true } }); //임시 경로
     };
     return (
         <CategoryLayout position={position}>
@@ -30,8 +58,8 @@ const Category = (position) => {
                 return (
                     <div
                         key={idx}
-                        onClick={(idx) => {
-                            onClickHandler(idx);
+                        onClick={() => {
+                            onClickHandler(item);
                         }}
                     >
                         {item}
@@ -51,6 +79,8 @@ const CategoryLayout = styled.div`
     gap: 20px;
     align-items: center;
     justify-content: center;
+    overflow-y: scroll;
+    padding-bottom: 100px;
     //margin-left: 30px;*/
     & > div {
         display: flex;
