00e76d1652ef7e16166e561d3fb0e3bf961af6c5
hae_oo <songhj10207@naver.com>
2024-08-29 16:37:55 +0900

    fix : API 수정에 따른 코드 수정, 무한스크롤 버그 수정

--- src/apis/api/storeList.jsx
+++ src/apis/api/storeList.jsx
@@ -1,6 +1,6 @@
 import { defaultInstance } from '../utils/instance';
 
-//category 검색 가게 리스트
+//검색 가게 리스트
 export const getStoreList = async (params) => {
     console.log(params);
     try {
@@ -17,43 +17,27 @@ export const getStoreList = async (params) => {
         };
     }
 };
-//nearby_station 검색 가게 리스트
-export const getStationStoreList = async (params) => {
-    try {
-        const { data, status } = await defaultInstance.get(`api/stores/search/nearby_station`, { params: params });
-        return {
-            data,
-            status,
-        };
-    } catch (e) {
-        console.log(e);
-        return {
-            error: e.response.data.detail,
-            status: e.response.status,
-        };
-    }
-};
-//name 검색 가게리스트
-export const getNameStoreList = async (params) => {
-    try {
-        const { data, status } = await defaultInstance.get(`api/stores/search/name`, { params: params });
-        return {
-            data,
-            status,
-        };
-    } catch (e) {
-        console.log(e);
-        return {
-            error: e.response.data.detail,
-            status: e.response.status,
-        };
-    }
-};
+
 // 가게 상세 정보
 export const getStoreDetail = async (id) => {
-    console.log('Store ID:', id);
     try {
         const { data, status } = await defaultInstance.get(`/api/stores/${id.storeId}`);
+        return {
+            data,
+            status,
+        };
+    } catch (e) {
+        console.log(e);
+        return {
+            error: e.response.data.detail,
+            status: e.response.status,
+        };
+    }
+};
+
+export const getStoreAll = async (params) => {
+    try {
+        const { data, status } = await defaultInstance.get(`/api/stores/all`, { params: params });
         return {
             data,
             status,
