3f0ad352b6b892d4f545462286f859e7a2bd765b
songhj10207@naver.com <songhj10207@naver.com>
2024-09-09 22:06:10 +0900

    feat : 필터링 기능 추가 #35

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,8 @@
-import Category from './Category';
-import MyMap from './Map';
+import Category from './category/Category';
+import MyMap from './map/Map';
 import StoreDetail from './StoreDetail';
-import StoreList from './StoreList';
-import StoreCard from './StoreCard';
-import MobileNav from './MobileNav';
-export { Category, MyMap, StoreDetail, StoreList, StoreCard, MobileNav };
+import StoreList from './store/StoreList';
+import StoreCard from './store/StoreCard';
+import MobileNav from './nav/MobileNav';
+import Filtering from './filtering/Filtering';
+export { Category, MyMap, StoreDetail, StoreList, StoreCard, MobileNav, Filtering };
