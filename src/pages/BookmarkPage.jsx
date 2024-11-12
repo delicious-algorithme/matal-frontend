5ef8e25cf2af7044196b6a60124f8483f0b901fb
songhj10207@naver.com <songhj10207@naver.com>
2024-11-12 11:05:47 +0900

    feat: 북마크 api 추가

---
+++ src/pages/BookmarkPage.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { StoreCard } from '../components/storeCard';
+
+const BookmarkPage = () => {
+    return <BookmarkLayout></BookmarkLayout>;
+};
+
+export default BookmarkPage;
+
+const BookmarkLayout = styled.div``;
