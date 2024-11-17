8f3171ef73406e9daff3983cf435c4b6450abfb8
songhj10207@naver.com <songhj10207@naver.com>
2024-11-17 13:04:26 +0900

    fix: 삭제, 추가 기능 후 북마크 요청하는 함수 추가

--- src/components/common/bookmark/BookmarkContainer.jsx
+++ src/components/common/bookmark/BookmarkContainer.jsx
@@ -3,14 +3,14 @@ import { ReactComponent as SavedBookmarkIcon } from '../../../assets/Icon/detail
 import { deleteBookmarkStore, postBookmarkStore } from '../../../apis/api/bookmarks';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { getBookmarksStores } from '../../../apis/api/bookmarks';
 import { useSaveBookmarkId } from '../../../store';
-import { useState } from 'react';
 
 const BookmarkContainer = ({ bookmarkId, storeId }) => {
-    const { savedId } = useSaveBookmarkId();
+    const { savedId, setBookmarkStore } = useSaveBookmarkId();
 
     const auth = JSON.parse(localStorage.getItem('auth')) || {};
-    const [isSaved, setIsSaved] = useState(savedId.includes(bookmarkId) && auth.state.isLoggedIn);
+    const isSaved = savedId.includes(bookmarkId) && auth.state.isLoggedIn;
     const navigate = useNavigate();
 
     const handleClickBookmarks = async (e) => {
@@ -24,18 +24,38 @@ const BookmarkContainer = ({ bookmarkId, storeId }) => {
                 const response = await deleteBookmarkStore(bookmarkId);
                 if (response.status === 204) {
                     console.log('success delete');
-                    setIsSaved(false);
+                    fetchBookmarkStores();
                 }
             } else {
                 const response = await postBookmarkStore(storeId);
                 if (response.status === 201) {
-                    setIsSaved(true);
+                    fetchBookmarkStores();
                 } else {
                     console.log(response.error);
                 }
             }
         } catch (error) {
             console.log('bookmark:', error);
+        }
+    };
+
+    const fetchBookmarkStores = async () => {
+        const auth = JSON.parse(localStorage.getItem('auth')) || {};
+        if (!auth.state.isLoggedIn) {
+            navigate('/login');
+        }
+        try {
+            const response = await getBookmarksStores();
+            if (response.status === 200) {
+                const newData = response.data;
+                setBookmarkStore([...newData]);
+            } else {
+                navigate('/login');
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            console.log('success getBookmark');
         }
     };
 
