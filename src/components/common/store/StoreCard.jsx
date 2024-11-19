cabba745bb542d418714966966b2f35fce4ddba1
songhj10207@naver.com <songhj10207@naver.com>
2024-11-19 13:15:16 +0900

    fix : 상세 페이지 레이아웃 수정

--- src/components/common/store/StoreCard.jsx
+++ src/components/common/store/StoreCard.jsx
@@ -37,13 +37,15 @@ const StoreListCard = ({ image, alt, id, name, address, rating, positiveRatio, p
                             </p>
                         </PositiveRatioContainer>
                     </RatingContainer>
-                    <PositiveKeywordsBox>
-                        <p># {positiveKeywords}</p>
-                    </PositiveKeywordsBox>
-                    <LocationBox>
-                        <LocationOn />
-                        <p>{address}</p>
-                    </LocationBox>
+                    <KeywordAndLocationBox>
+                        <PositiveKeywordsBox>
+                            <p># {positiveKeywords}</p>
+                        </PositiveKeywordsBox>
+                        <LocationBox>
+                            <LocationOn />
+                            <p>{address}</p>
+                        </LocationBox>
+                    </KeywordAndLocationBox>
                 </ContentsBox>
             </ContentsContainer>
         </StoreListCardLayout>
@@ -68,21 +70,21 @@ const StoreListCardLayout = styled.div`
     @media screen and (max-width: 500px) {
         flex-direction: column;
         align-items: flex-start;
-        height: 300px;
+        height: 160px;
     }
 
     @media screen and (max-width: 350px) {
         margin-left: 10px;
+        height: 160px;
     }
 `;
 
 const ContentsContainer = styled.div`
     display: flex;
     flex-direction: row;
-    @media screen and (max-width: 500px) {
-        flex-direction: column;
-    }
 `;
+
+const KeywordAndLocationBox = styled.div``;
 
 const ImageContainer = styled.div`
     width: 150px;
@@ -90,9 +92,10 @@ const ImageContainer = styled.div`
     overflow: hidden;
     flex-shrink: 0;
     border-radius: 10px;
+    display: flex;
     @media screen and (max-width: 500px) {
-        width: 300px;
-        height: 160px;
+        width: 100px;
+        height: 100px;
     }
 `;
 
@@ -108,6 +111,9 @@ const ContentsBox = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
+    @media screen and (max-width: 350px) {
+        width: 200px;
+    }
 `;
 
 const LocationBox = styled.div`
@@ -134,6 +140,13 @@ const NameAndBookmarkContainer = styled.div`
     & > p {
         font-weight: 700;
         font-size: 19px;
+    }
+
+    @media screen and (max-width: 500px) {
+        & > p {
+            font-weight: 700;
+            font-size: 16px;
+        }
     }
 `;
 
