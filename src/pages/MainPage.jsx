d6cde2bfd35830ccf710139b151cceda14d2d38e
songhj10207@naver.com <songhj10207@naver.com>
2024-11-25 01:11:10 +0900

    feat: 첫 페이지 렌더링 시 북마크 불러오는 기능 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,18 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as Banner } from '../assets/Icon/banner/Banner.svg';
-import { useIsFetch } from '../store';
+import { useIsFetch, useSaveBookmarkId } from '../store';
 import { CategoryContainer, SearchKeyword, TopRecommendations, MapContainer } from '../components/main';
 import { SearchBar, Button, Footer } from '../components/common';
+import { getBookmarksStores } from '../apis/api/bookmarks';
 
 const MainPage = () => {
     const [searchInput, setSearchInput] = useState();
     const navigate = useNavigate();
+    const [stores, setStores] = useState();
     const { setIsFetchAll } = useIsFetch();
+    const { setBookmarkStore, setSaveBookmarkId } = useSaveBookmarkId();
 
     const onChangeHandler = (e) => {
         setSearchInput(e.target.value);
@@ -29,8 +32,49 @@ const MainPage = () => {
         }
     };
 
+    useEffect(() => {
+        const auth = JSON.parse(localStorage.getItem('auth')) || {};
+        if (auth.state.isLoggedIn) {
+            getAllBookmarksStores();
+        }
+        // eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
+        if (stores.length > 0) {
+            const saveBookmarkId = stores.map((store) => store.bookmarkId);
+            const bookmarkIds = [...new Set(saveBookmarkId)];
+            setSaveBookmarkId(bookmarkIds);
+        }
+        // eslint-disable-next-line
+    }, [stores]);
+
     const buttonClickHandler = () => {
         navigate('/webmap');
+    };
+
+    const getAllBookmarksStores = async () => {
+        try {
+            let page = 0;
+            let allData = [];
+            let hasMoreData = true;
+            while (hasMoreData) {
+                const response = await getBookmarksStores(page);
+                if (response.status === 200) {
+                    allData = allData.concat(response.data.content);
+                    hasMoreData = response.data.last !== true;
+                    page++;
+                } else {
+                    navigate('/login');
+                }
+            }
+            setBookmarkStore(allData);
+            setStores(allData);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            console.log('success getBookmark');
+        }
     };
 
     return (
